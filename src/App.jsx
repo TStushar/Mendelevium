634a2dd79530a5e8bde0bfd93e8f6bdf9bb26b95
Ganesh Jamadar <23f3003450@ds.study.iitm.ac.in>
2025-02-08 23:58:15 +0530

    added user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import './index.css'; 
-import { UserProfile } from './ui-user-profile';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Network_tab from './ui-network/NetworkTab.jsx';
-
-
-
+import React from "react";
+import "./index.css";
+import { UserProfile } from "./ui-user-profile";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Network_tab from "./ui-network/NetworkTab.jsx";
 
 const App = () => {
-  return <>
-  {/* <UserProfile/> */}
-  <Router>
-    
-    <Routes>
-      <Route path="/network_tab" element={<Network_tab/>} />
-     
-    </Routes>
-  </Router>
-</>
-}
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/network_tab" element={<Network_tab />} />
+          <Route path="/" element={<UserProfile />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
-
